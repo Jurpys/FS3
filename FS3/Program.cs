47d121ac528f843ce7d99f6c8141376907cdf9db
Jurpys <jurgaitis.mantas@gmail.com>
2015-12-15 15:17:29 +0200

    123

--- FS3/Program.cs
+++ FS3/Program.cs
@@ -18,12 +18,7 @@ namespace FS3
 
             //var message = Maybe<string>.Bind("d1:0d1:v1:x1:xi2e1:yi1ee1:1d1:v1:o1:xi1e1:yi2ee1:2d1:v1:x1:xi0e1:yi0ee1:3d1:v1:o1:xi0e1:yi2ee1:4d1:v1:x1:xi2e1:yi0ee1:5d1:v1:o1:xi2e1:yi2eee");
 
-            //Func<string, Dictionary<string, Dictionary<string, string>>> decoderToExternalMap = MessageDecoder.Execute;
-            //Func<Dictionary<string, Dictionary<string, string>>, List<Tuple<int, int>>> decoderToCoordinates =
-            //    MessageDecoder.DecodeToCoordinates;
-            //Func<List<Tuple<int, int>>, Tuple<int, int>> randomFinder = MoveFinder.GetRandomMove;
-
-            //var winner = message.FMap(decoderToExternalMap).FMap(decoderToCoordinates).FMap(randomFinder);
+            //var winner = message.FMap(MessageDecoder.Execute).FMap(MessageDecoder.DecodeToCoordinates).FMap(MoveFinder.GetRandomMove);
 
             //Console.WriteLine(winner.ToString());
             //Console.ReadLine();
